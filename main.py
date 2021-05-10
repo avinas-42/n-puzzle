3467886801ad699cf26ac71f7aed16f7844ce763
arnauz94 <kil94@live.fr>
2021-05-10 19:10:49 +0200

    get goal

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 ﻿import sys
 from classes.CState import CState
 from classes.CNode import CNode
-
+from classes.CPuzzle import CPuzzle
 
 def main(argv):
     table = [8,1,4,3,0,5,7,6,2]
     state = CState(size=3, table=table)
     node = CNode(state, 0, 0)
+    puzzle = CPuzzle(3)
+
     print(node)
-    print(node.getChildren())
+    node.getChildren()
+
+    for child in node.getChildren()
+
     
    
 if __name__ == "__main__":
