31e904a356ef5cb74ca79cb8d81d4f213d6665ca
arnauz94 <kil94@live.fr>
2021-05-10 16:48:49 +0200

    swap table

--- Classes/CState.py
+++ Classes/CState.py
@@ -1,1 +1,25 @@
-﻿
+﻿class CState :
+    def __init__(self, size, table):
+        self.size = size
+        self.table = table
+    
+    def swap(self, x1, y1, x2, y2):
+        self.table[x1 + y1 * self.size] = self.table[x1 + y1 * self.size] + self.table[x2 + y2 * self.size]
+        self.table[x2 + y2 * self.size] = self.table[x1 + y1 * self.size] - self.table[x2 + y2 * self.size] 
+        self.table[x1 + y1 * self.size] = self.table[x1 + y1 * self.size] - self.table[x2 + y2 * self.size]
+    
+    def getTile(self, x, y):
+        return self.table[x + y * self.size]
+
+    def setTile(self, x, y, value):
+        self.table[x + y * self.size] = value
+
+
+    def __str__(self):
+        ret = str(self.size) +"\n|"
+        for i in range(len(self.table)):
+            ret += "\t" + str(self.table[i]) + "\t|"
+            if ((i + 1) % self.size == 0 and i + 1 < len(self.table) ) :
+                ret += "\n|"
+        return ret
+        
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-﻿
+﻿import sys
+from classes.CState import CState
+
+def main(argv):
+    table = [8,1,4,3,0,5,7,6,2]
+    state = CState(size=3, table=table)
+    print(state)
+    state.swap(1,1,2,1)
+    print (state)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
