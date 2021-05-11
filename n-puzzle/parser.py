1a85ad799b3c74979cbd0eb623b861df7b71063c
anaroste <tegui1756@gmail.com>
2021-05-11 09:18:55 +0200

    parser + generator

---
+++ n-puzzle/parser.py
@@ -0,0 +1,28 @@
+def parsing(fileName):
+    size = None
+    taquin = []
+    with open(fileName, 'r') as file:
+        for line in file:
+            if line[0] == '#':
+                continue
+            if line.find('#') != -1:
+                line = line[:line.find('#')]
+            tabLine = line.split(' ')
+            if len(tabLine) != 1 and size != None:
+                for nb in tabLine:
+                    if nb == '':
+                        continue
+                    taquin.append(int(nb.replace('\n', '')))
+            else:
+                size = int(tabLine[0].replace('\n', ''))
+
+    if len(taquin) != size * size:
+        return "error", "La taille du taquin ne correspond pas avec le nombre de tuile."
+    for nb in range(0, size * size):
+        if nb in taquin:
+            continue
+        else:
+            return "error", "La valeur d'une tuile du taquin ne correspond pas."
+    return size, taquin
+
+print(parsing("puzzle.txt")[1])
