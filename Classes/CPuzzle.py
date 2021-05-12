918cc6c35ba1355fa5ab9479e783891ba13976ab
arnauz94 <kil94@live.fr>
2021-05-12 16:51:24 +0200

    manathan

--- Classes/CPuzzle.py
+++ Classes/CPuzzle.py
@@ -1,15 +1,58 @@
 ﻿from pynput import keyboard
+from .CState import CState
 def getKey(obj):
     return obj.fScore
 class CPuzzle :
     def __init__(self,size,startNode):
         self.startNode = startNode
         self.size = size
-        self.goal = list(range(1,size*size))
-        self.goal.append(0)
+        self.goal = self.getGoal(size)
         self.open = []
         self.close = []
         self.listen = True
+
+    def nextdirection(self, direction) :
+        if direction + 1 > 3 :
+            direction = 0
+        else :
+            direction += 1
+        return direction
+
+    def forward(self, direction, pos) :
+        nextPos = [0,0]
+        nextPos[0] = pos[0]
+        nextPos[1] = pos[1]
+        if direction == 0 :
+            nextPos[0] = pos[0] + 1 
+        elif direction == 1 :
+            nextPos[1] = pos[1] + 1
+        elif direction == 2 :
+            nextPos[1] = pos[1] - 1 
+        elif direction == 3 :
+            nextPos[0] = pos[0] - 1
+        return nextPos
+
+    def getGoal(self, size) :
+        table = [0] * (self.size * self.size)
+        state = CState(size, table = table)
+        pos = [0, 0]
+        direction = 0
+        nextPos = [0,0]
+        value = 1
+
+        while state.table.count(0) > 1 :            
+            if nextPos[0] >= 0 and nextPos[0] < self.size and nextPos[1] >= 0 and nextPos[1] < self.size :
+                if state.getTile(nextPos[0], nextPos[1]) == 0 :
+                    pos[0] = nextPos[0]
+                    pos[1] = nextPos[1]
+                    state.setTile(pos[0], pos[1], value)
+                    value += 1
+                else :
+                    direction = self.nextdirection(direction)
+            else :
+                direction = self.nextdirection(direction)
+            nextPos = self.forward(direction ,pos)
+        return state
 
     def play(self) :
         print(self.startNode.state)
@@ -46,28 +89,32 @@ class CPuzzle :
             pass
     
     def execution(self) :
-        tabChild = self.startNode.getChildren(self.goal)
+        tabChild = self.startNode.getChildren(self.goal.table)
         tabChild.sort(key=getKey)
         self.open += tabChild
         self.close.append(self.startNode)
         cpt = 0
         while 1:
             cpt += 1 
-            # if cpt % 500 == 0:
+            # if cpt % 2000 == 0:
             #     print(self.open[0])
             if self.open[0].fScore - self.open[0].level == 0 or len(self.open) == 0:
                 break
-            tabChild = self.open[0].getChildren(self.goal)
+            tabChild = self.open[0].getChildren(self.goal.table)
             for elem in self.close:    
                 for child in tabChild:
                     if child.state.table == elem.state.table:
                             tabChild.remove(child)
+            for elem in self.open :
+                for child in tabChild:
+                    if child.state.table == elem.state.table :
+                            tabChild.remove(child)
+            
             self.open += tabChild
             self.close.append(self.open[0])
             self.open.pop(0)
             self.open.sort(key=getKey)
-            print(self.open)
-            print("----------------------------------")
+
         elem = self.open[0]
         while elem.daddy != None:
             print(elem)
